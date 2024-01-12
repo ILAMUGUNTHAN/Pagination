ea7e057b993c09d96786037beac21a5d15c023c9
ILAMUGUNTHAN N <86196624+ILAMUGUNTHAN@users.noreply.github.com>
2024-01-12 17:59:57 +0530

    Add files via upload

---
+++ Page Nation/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Page_Nation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.EnterBtn = new System.Windows.Forms.Button();
+            this.backBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.restartBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(58, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Total Pages:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(205, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(102, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(207, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "View Pages:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Location = new System.Drawing.Point(83, 367);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(639, 59);
+            this.panel1.TabIndex = 4;
+            // 
+            // EnterBtn
+            // 
+            this.EnterBtn.BackColor = System.Drawing.Color.Teal;
+            this.EnterBtn.Location = new System.Drawing.Point(56, 80);
+            this.EnterBtn.Name = "EnterBtn";
+            this.EnterBtn.Size = new System.Drawing.Size(251, 49);
+            this.EnterBtn.TabIndex = 5;
+            this.EnterBtn.Text = "Enter";
+            this.EnterBtn.UseVisualStyleBackColor = false;
+            // 
+            // backBtn
+            // 
+            this.backBtn.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.backBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.backBtn.Location = new System.Drawing.Point(13, 369);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(64, 50);
+            this.backBtn.TabIndex = 0;
+            this.backBtn.Text = "<";
+            this.backBtn.UseVisualStyleBackColor = false;
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.nextBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nextBtn.Location = new System.Drawing.Point(728, 369);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(64, 50);
+            this.nextBtn.TabIndex = 1;
+            this.nextBtn.Text = ">";
+            this.nextBtn.UseVisualStyleBackColor = false;
+            // 
+            // restartBtn
+            // 
+            this.restartBtn.BackColor = System.Drawing.Color.Red;
+            this.restartBtn.Location = new System.Drawing.Point(618, 20);
+            this.restartBtn.Name = "restartBtn";
+            this.restartBtn.Size = new System.Drawing.Size(159, 87);
+            this.restartBtn.TabIndex = 6;
+            this.restartBtn.Text = "Restart";
+            this.restartBtn.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(199, 151);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(410, 191);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.restartBtn);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.EnterBtn);
+            this.Controls.Add(this.backBtn);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button EnterBtn;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Button backBtn;
+        private System.Windows.Forms.Button restartBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Page Nation/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Page_Nation
+{
+
+    
+    public partial class Form1 : Form
+    {
+        private int totalBtns ;
+        private int viewBtns ;
+        private int nextCount = 1;
+        private int curBtn=0;
+
+        Button b = new Button();
+        private Point sLoc = new Point(70, 3);
+        private Point absLoc;
+        List<Button> buttons = new List<Button>();
+
+        private string[] locArray = new string[] { "C:/Users/Illamugundhan/Desktop/pictures/pic1.jpg", "C:/Users/Illamugundhan/Desktop/pictures/pic2.jpg", "C:/Users/Illamugundhan/Desktop/pictures/pic3.jpg", "C:/Users/Illamugundhan/Desktop/pictures/pic4.jpg" };
+
+        private delegate void newDelegate(object obj,int n);
+        private event newDelegate display;
+        List<picture> pList = new List<picture>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            EnterBtn.Click += OnClickEnterBtn;
+            restartBtn.Click += OnClickRestart;
+            nextBtn.Click += OnClickNextBtn;
+            backBtn.Click += OnClickBackBtn;
+            display += ShowName;
+
+
+            
+        }
+
+        private void OnClickEnterBtn(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            nextCount = 1;
+            curBtn = 0;
+
+            //Button b = new Button();
+            sLoc = new Point(70, 3);
+            
+            buttons = new List<Button>();
+
+            if(textBox1.Text != "" && textBox2.Text!="" && (Convert.ToInt32(textBox1.Text)>= Convert.ToInt32(textBox2.Text)))
+            {
+                totalBtns = Convert.ToInt32(textBox1.Text);
+                viewBtns = Convert.ToInt32(textBox2.Text);
+
+                sLoc.X = (((panel1.Width - (((Convert.ToInt32(viewBtns+2)))) * 64))/2 ) -25;
+                absLoc = sLoc;
+                absLoc.X -= 64;
+
+                for(int i=0;i<totalBtns;i++)
+                {
+                    picture p = new picture();
+                    p.ID = i+1;
+                    p.Location = locArray[i % 4];
+
+                    pList.Add(p);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Enter valid inputs");
+                return;
+            }
+            if(viewBtns>7)
+            {
+                MessageBox.Show("Enter view button lesser than 7");
+                return;
+            }
+
+            b = new Button
+            {
+                Location = sLoc,
+                Size = new Size(64, 50),
+                Text = "..."
+            };
+            b.Click += OnClickBtn;
+            panel1.Controls.Add(b);
+            buttons.Add(b);
+            sLoc.X += 70;
+            b.Visible = false;
+            for (int i=1;i<=viewBtns;i++)
+            {
+                b = new Button
+                {
+                    Location = sLoc,
+                    Size = new Size(64, 50),
+
+                    Text = i + ""
+                };
+                b.Click += OnClickBtn;
+                panel1.Controls.Add(b);
+                buttons.Add(b);
+                sLoc.X += 70;
+
+            }
+            b = new Button
+            {
+                Location = sLoc,
+                Size = new Size(64, 50),
+                Text = "..."
+            };
+            b.Click += OnClickBtn;
+            panel1.Controls.Add(b);
+            buttons.Add(b);
+            sLoc.X += 70;
+            //buttons[0].Focus();
+
+
+
+            //for (int i=totalBtns-viewBtns+1;i<=totalBtns;i++)
+            //{
+            //    b = new Button
+            //    {
+            //        Location = sLoc,
+            //        Size = new Size(64, 50),
+
+            //        Text = i + ""
+
+            //    };
+            //    b.Click += OnClickBtn;
+            //    panel1.Controls.Add(b);
+            //    buttons.Add(b);
+            //    sLoc.X += 70;
+            //}
+        }
+
+        private void OnClickRestart(object sender, EventArgs e)
+        {
+            this.Close();
+
+            System.Diagnostics.Process.Start(Application.ExecutablePath);
+        }
+
+        
+        int ctrB = 0;
+        private void OnClickNextBtn(object sender , EventArgs e)
+        {
+            //curBtn++;
+            //int startBtnIter = curBtn;
+            //for(int ctr=0;ctr<viewBtns;ctr++)
+            //{
+            //    buttons[ctr + 1].Text = startBtnIter + "";
+            //}
+            if (ctrB == totalBtns)
+            {
+                return;
+            }
+            int a = nextCount;
+            if (buttons[viewBtns].Text == totalBtns + "")
+            {
+                buttons[viewBtns].Focus();
+
+                return;
+            }
+            //for(int i=0;i<viewBtns;i++)
+            //{
+            //    buttons[i].Text = (Convert.ToInt32((buttons[i]).Text) + 1 )+"";
+
+            //}
+            //nextCount++;
+
+            if (buttons[nextCount - 1].Text == Convert.ToString(viewBtns))
+            {
+                buttons[0].Visible = true;
+            }
+            if (buttons[buttons.Count - 2].Text == (totalBtns - 1 + ""))
+            {
+                buttons[buttons.Count - 1].Visible = false;
+            }
+            if (ctrB >= totalBtns)
+            {
+                //ctr = totalBtns;
+                buttons[buttons.Count - 2].Focus();
+                //ShowName(null, buttons.Count-2);
+                return;
+            }
+            if (nextCount == viewBtns + 1)
+            {
+                for (int i = 1; i <= viewBtns; i++)
+                {
+
+                    buttons[i].Text = (Convert.ToInt32((buttons[i]).Text) + 1) + "";
+                }
+                buttons[nextCount - 1].Focus();
+                //ShowName(null, nextCount-1);
+                //nextCount = 0;
+
+            }
+            else
+            {
+                buttons[nextCount].Focus();
+                //ShowName(null, nextCount);
+                nextCount++;
+            }
+            ctrB++;
+            //if (ctrB >= totalBtns)
+            //    nextCount--;
+
+            picfocus();
+
+
+
+        }
+        bool isL = false;
+        private void OnClickBackBtn(object sender, EventArgs e)
+        {
+
+            
+            //if (nextCount < 1) nextCount++;
+            if (ctrB<=1)
+            {
+                buttons[1].Focus();
+                return;
+            }
+
+            if(nextCount==2)
+            {
+                for(int i=1;i<=viewBtns;i++)
+                {
+                    buttons[i].Text = (Convert.ToInt32((buttons[i]).Text) -1 ) + "";
+                }
+                buttons[1].Focus();
+            }
+            else
+            {
+                buttons[nextCount - 1].Focus();
+                //ShowName(null, (isL) ? nextCount - 1 : nextCount - 2);
+                nextCount--;
+                //isL = false;
+            }
+            ctrB--;
+            if (buttons[1].Text == (totalBtns - viewBtns + ""))
+            {
+                buttons[buttons.Count - 1].Visible = true;
+            }
+            if (buttons[1].Text == "1")
+            {
+                buttons[0].Visible = false;
+            }
+
+            picfocus();
+        }
+
+        private void OnClickBtn(object sender, EventArgs e)
+        {
+            if (int.TryParse((sender as Button).Text, out _))
+            {
+
+                display(sender, Convert.ToInt32((sender as Button).Text));
+                //(sender as Button).Focus();
+                curBtn = Convert.ToInt32((sender as Button).Text);
+                for (int i = 0; i < buttons.Count; i++)
+                {
+                    if (buttons[i].Text == (sender as Button).Text)
+                    {
+                        nextCount = i + 1;
+                    }
+                }
+                //nextCount = curBtn%viewBtns -1;
+                //ctr = curBtn;
+                ctrB = Convert.ToInt32((sender as Button).Text);
+                isL = false;
+                picfocus();
+
+
+            }
+            else
+            {
+                if((sender as Button)==buttons[buttons.Count-1])
+                {
+                    int val = totalBtns;
+                    for(int i=buttons.Count-2;i>0;i--)
+                    {
+                        buttons[i].Text = val-- +"";
+                        
+                    }
+                    buttons[buttons.Count - 1].Visible = false;
+                    buttons[0].Visible = true;
+                    nextCount = viewBtns;
+                    ctrB = totalBtns-1;
+                    isL = true;
+                    buttons[nextCount].Focus();
+                    picfocus();
+
+
+                }
+                else
+                {
+                    //int val = viewBtns;
+                    for (int i = 1; i<=viewBtns; i++)
+                    {
+                        buttons[i].Text = i + "";
+
+                    }
+                    buttons[0].Visible = false;
+                    buttons[buttons.Count - 1].Visible = true;
+                    nextCount = 2;
+                    ctrB = 1;
+                    picfocus();
+                }
+            }
+        }
+        private void ShowName(object sender, int i)
+        {
+            //MessageBox.Show("Page"+i);
+
+            //pictureBox1.Image = Image.FromFile(pList[i-1].Location);
+            //pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+        private void picfocus()
+        {
+            int id = 0;
+            
+            foreach (Button btn in buttons)
+            {
+                if (btn.Focused)
+                {
+                    if (int.TryParse(btn.Text, out id)) ;
+                    break;
+                }
+            }
+            if (id > 0)
+            {
+                
+                if (pictureBox1.Image!=null)
+                {
+                    Image i = pictureBox1.Image;
+                    i.Dispose();
+                }
+                pictureBox1.Image = Image.FromFile(pList[id - 1].Location);
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+    }
+}
